b426392e6b7674d94747a84a6fac9e3cb4009c58
ZacharyCWilliams <34009260+ZacharyCWilliams@users.noreply.github.com>
2018-04-30 19:50:01 -0700

    Update Foursquare.js

--- src/Foursquare.js
+++ src/Foursquare.js
@@ -2,37 +2,19 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
 
-
+// https://github.com/foursquare/react-foursquare
 var foursquare = require('react-foursquare')({
   clientID: 'redacted',
   clientSecret: 'redacted'
 });
 
-// var params = [
-//   {"ll": "37.395208,-122.079159",
-//   "query": '23 And Me'},
-//   {'ll': '37.359605, -122.066855',
-//   'query': 'Mountain View High School'},
-//   {"ll": "37.422000, -122.084057",
-//   "query": 'Google'},
-//   {'ll': '37.427475, -122.169719',
-//   'query': 'Stanford University'},
-//   {"ll": "37.398900, -122.110727",
-//   "query": 'Whole Foods'},
-//   {'ll': '37.369124, -122.079870',
-//   'query': 'El Camino Hospital'},
-//   {"ll": "37.378540, -122.116718",
-//   "query": 'Starbucks'},
-//   {'ll': '37.408564, -122.179599',
-//   'query': 'Stanford Dish'},
-//   {"ll": "37.377386, -122.031401",
-//   "query": 'Philz Coffee'},
-//   {'ll': '37.360278, -122.126562',
-//   'query': 'Foothill College'}
-// ];
-
-// get JSON request of foursquare data
-
+let params = {
+  "ll": "37.7749,-122.4194",
+  "query": 'Blue Bottle'
+};
+let params2 = {
+  'venue_id': '5560dbdb498e91a2bcde84f6'
+}
 
 
 class Foursquare extends Component {
@@ -41,24 +23,31 @@ class Foursquare extends Component {
      super(props);
      this.state = {
        items: [],
+       // state passed in from app component
        latlng: {'ll': this.props.lattitude.toString() + ',' + this.props.longitude.toString(),
      'query': this.props.titleQuery}
      };
    }
 
+
+   // make venue detail api request and set as item state
   componentDidMount() {
-    console.log('we are now inside componentDidMount')
-    foursquare.venues.getVenues(this.state.latlng)
-      .then(res=> {
-        console.log(res);
-        this.setState({ items: res.response.venues });
-        console.log('items: ', this.state.items);
+    foursquare.venues.getVenue(params2)
+      .then(res => {
+        console.log(res)
+        rejection => console.log(rejection)
+        this.setState({ items: res.venue });
       });
   }
 
+  render() {
+    console.log('foursquare.venues.getVenue: ', foursquare.venues.getVenue)
+    console.log('foursquare.venues.getVenue(params2)', foursquare.venues.getVenue(params2))
+    console.log('this.state.items: ', this.state.items)
 
-  render() { (
+    return (
     <div>
+    <div ref='testin'></div>
         { this.state.items.map(item=> { return <div key={item.id}>{item.name}</div>}) }
     </div>
   )
