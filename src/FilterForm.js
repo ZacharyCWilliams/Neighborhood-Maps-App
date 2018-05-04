0c85ad434ec35d25a2a9314a8dd63a33a8d112fb
ZacharyCWilliams <34009260+ZacharyCWilliams@users.noreply.github.com>
2018-05-03 18:49:59 -0700

    Create FilterForm.js

---
+++ src/FilterForm.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import ReactDOMServer from 'react-dom/server';
+import ReactDOM from 'react-dom';
+import './App.css';
+
+
+class FilterForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ value: this.props.selectors })
+    console.log('the begginning state is ', this.state.value)
+  }
+
+  handleFilter(event) {
+    console.log(event.target.value)
+  }
+
+
+  render() {
+    if (this.state.value){
+      console.log('state is loaded! ', this.state.value);
+      let firstVenue = this.state.value[0].title;
+    }
+
+    return (
+      <div>
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" onChange={this.handleFilter} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+        <select>
+          <option value="v.tit">Strawberry</option>
+          <option value="lime">Lime</option>
+          <option value="coconut">Coconut</option>
+          <option value="mango">Mango</option>
+        </select>
+      </div>
+  )
+  }
+}
+
+export default FilterForm;
