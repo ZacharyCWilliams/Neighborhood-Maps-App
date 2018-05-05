5f45265a64adf5a1cb2d8c44ebd6674fb51f7219
ZacharyCWilliams <34009260+ZacharyCWilliams@users.noreply.github.com>
2018-05-05 11:15:31 -0700

    Update FilterForm.js

--- src/FilterForm.js
+++ src/FilterForm.js
@@ -3,45 +3,56 @@ import ReactDOMServer from 'react-dom/server';
 import ReactDOM from 'react-dom';
 import './App.css';
 
-
 class FilterForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: null
+      value: this.props.selectors
     };
-  }
-
-  componentDidMount() {
-    this.setState({ value: this.props.selectors })
+    this.handleFilter = this.handleFilter.bind(this)
   }
 
   handleFilter(event) {
-    console.log(event.target.value)
+      var input, filter, ul, li, a, i;
+      input = document.getElementById("myInput");
+      filter = input.value.toUpperCase();
+      ul = document.getElementById("myUL");
+      li = ul.getElementsByTagName("li");
+      for (i = 0; i < li.length; i++) {
+          a = li[i].getElementsByTagName("a")[0];
+          if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+              li[i].style.display = "";
+          } else {
+              li[i].style.display = "none";
+
+          }
+      }
   }
 
+  handleOnClick() {
+    console.log('this worked')
+  }
 
   render() {
-    if (this.state.value){
-      console.log('state is loaded! ', this.state.value);
-      let firstVenue = this.state.value[0].title;
-    }
-
     return (
       <div>
       <form onSubmit={this.handleSubmit}>
         <label>
-          Name:
-          <input type="text" onChange={this.handleFilter} />
+          <input type="text" id="myInput" onChange={this.handleFilter} />
+          <ul id="myUL">
+            <li className="myLi"><a onClick={this.handleOnClick}>{this.state.value[0].title}</a></li>
+            <li className="myLi"><a onClick={this.handleOnClick}>{this.state.value[1].title}</a></li>
+            <li className="myLi"><a onClick={this.handleOnClick}>{this.state.value[2].title}</a></li>
+            <li className="myLi"><a onClick={this.handleOnClick}>{this.state.value[3].title}</a></li>
+            <li className="myLi"><a onClick={this.handleOnClick}>{this.state.value[4].title}</a></li>
+            <li className="myLi"><a onClick={this.handleOnClick}>{this.state.value[5].title}</a></li>
+            <li className="myLi"><a onClick={this.handleOnClick}>{this.state.value[6].title}</a></li>
+            <li className="myLi"><a onClick={this.handleOnClick}>{this.state.value[7].title}</a></li>
+            <li className="myLi"><a onClick={this.handleOnClick}>{this.state.value[8].title}</a></li>
+            <li className="myLi"><a onClick={this.handleOnClick}>{this.state.value[9].title}</a></li>
+          </ul>
         </label>
-        <input type="submit" value="Submit" />
       </form>
-        <select>
-          <option value="v.tit">Strawberry</option>
-          <option value="lime">Lime</option>
-          <option value="coconut">Coconut</option>
-          <option value="mango">Mango</option>
-        </select>
       </div>
   )
   }
