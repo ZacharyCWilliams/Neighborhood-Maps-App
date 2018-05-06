41268856b9eb05aa6e1e26fddd431209ab99b4d1
ZacharyCWilliams <34009260+ZacharyCWilliams@users.noreply.github.com>
2018-05-06 15:35:12 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,11 +4,30 @@ import ReactDOM from 'react-dom';
 import './App.css';
 import Foursquare from './Foursquare.js';
 import FilterForm from './FilterForm.js';
+import SelectOptions from './SelectOptions';
+
+let map;
+let markers = [];
+let marker;
+
+
 
 class App extends Component {
 
 state = {
-    googleMap: []
+    googleMap: [],
+    FilteredLocations: [
+      {title: '23 And Me', location: {lat: 37.395208, lng: -122.079159}, venueID: '4ae8903bf964a5206eb021e3'},
+      {title: 'Mountain View High School', location: {lat: 37.359605, lng: -122.066855}, venueID: '4acfcafff964a5200bd620e3'},
+      {title: 'Google', location: {lat: 37.422000, lng: -122.084057}, venueID: '50379c31e4b0be420ec1826a'},
+      {title: 'Stanford University', location: {lat: 37.427475, lng: -122.169719}, venueID: '4a983497f964a520f02a20e3'},
+      {title: 'Whole Foods', location: {lat: 37.398900, lng: -122.110727}, venueID: '49f8a2e3f964a5200d6d1fe3'},
+      {title: 'El Camino Hospital', location: {lat: 37.369124, lng: -122.079870}, venueID: '4a77b401f964a52004e51fe3'},
+      {title: 'Starbucks', location: {lat: 37.378540, lng: -122.116718}, venueID: '4740b317f964a520724c1fe3'},
+      {title: 'Stanford Dish', location: {lat: 37.408564, lng: -122.179599}, venueID: '4a8723a4f964a520cf0220e3'},
+      {title: 'Philz Coffee', location: {lat: 37.377386, lng: -122.031401}, venueID: '547e13a7498e8d4312025ce9'},
+      {title: 'Foothill College', location: {lat: 37.360278, lng: -122.126562}, venueID: '49f667e7f964a5203e6c1fe3'}
+    ]
   }
 
   componentDidMount() {
@@ -28,7 +47,7 @@ state = {
       });
       initMap(map, maps)
     });
-      let markers = [];
+
     //create map, map markers, and infowindows
     function initMap(map, maps) {
 
@@ -68,41 +87,46 @@ state = {
         marker.addListener('click', function(){
           populateInfoWindow(this, largeInfoWindow);
         });
+        //open and close infowindow on click + set foursquare component to infowindow content
+        function populateInfoWindow(marker, infowindow){
+          if (infowindow !== marker) {
+            infowindow.marker = marker;
+
+            let passDownParams = marker.position.lat();
+            let passDownLng = marker.position.lng();
+            let markerTitle = marker.title;
+            let placeID = marker.markerID;
+
+            let infowindowDiv = document.createElement('div');
+            let foursquareStuff = <Foursquare lattitude={passDownParams} longitude={passDownLng} titleQuery={markerTitle} theVenueID={placeID}/>;
+            ReactDOM.render(foursquareStuff, infowindowDiv);
+            infowindow.setContent( infowindowDiv );
+
+            infowindow.open(map, marker);
+             infowindow.addListener('closeclick', function() {infowindow.setContent(null);});
+           }
+          }
+
       }
       //make sure all markers fit on screen
       map.fitBounds(bounds);
-      //open and close infowindow on click + set foursquare component to infowindow content
-      function populateInfoWindow(marker, infowindow) {
-      if (infowindow !== marker) {
-        infowindow.marker = marker;
-
-        let passDownParams = marker.position.lat();
-        let passDownLng = marker.position.lng();
-        let markerTitle = marker.title;
-        let placeID = marker.markerID;
-        let infowindowDiv = document.createElement('div');
-        let foursquareStuff = <Foursquare lattitude={passDownParams} longitude={passDownLng} titleQuery={markerTitle} theVenueID={placeID}/>;
-        ReactDOM.render(foursquareStuff, infowindowDiv);
-        infowindow.setContent( infowindowDiv );
-          
-        infowindow.open(map, marker);
-         infowindow.addListener('closeclick', function() {infowindow.setContent(null);});
-       }
-       }
-
-       // filter function
-         let filterFormContent = <FilterForm selectors={markers} />
-         let filterFormDiv = document.getElementById('form');
-         ReactDOM.render(filterFormContent, filterFormDiv);
-
      };
+
      //if the map error's out push an alert to user that something went wrong
      function handleError(google) {
        if (google.error){
            alert('Oops! Looks like something went wrong.');
        }
      }
+
 }
+
+ handleLocationClick(venueID){
+  console.log(venueID);
+  const clickedMarker = markers.filter((marker) => marker.markerID === venueID);
+  console.log(clickedMarker[0].title);
+  // this.populateInfoWindow(clickedMarker[0]);
+ }
 
   render() {
 
@@ -112,7 +136,7 @@ state = {
           <h1 className="App-title">Neighborhood Maps Project</h1>
         </header>
         <div id='map'></div>
-        <div id='form'></div>
+        <FilterForm FilteredLocations={this.state.FilteredLocations} handleLocationClick={this.handleLocationClick} />
       </div>
     );
   }
