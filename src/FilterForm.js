89778b55e8485b8da89f6b50447eb52c0e8e2817
ZacharyCWilliams <34009260+ZacharyCWilliams@users.noreply.github.com>
2018-05-09 14:32:05 -0700

    Update FilterForm.js

--- src/FilterForm.js
+++ src/FilterForm.js
@@ -1,20 +1,18 @@
-import React, { Component } from 'react';
-import ReactDOMServer from 'react-dom/server';
-import ReactDOM from 'react-dom';
+import React from 'react';
 import './App.css';
 
 
 function FilterForm(props){
 
     let locationsList = props.FilteredLocations.map((location) =>
-									<li className="myLi" key={location.venueID} onClick={()=> props.handleLocationClick(location.venueID)}><a>{location.title}</a></li>
-								 );
+	<li className="myLi" key={location.venueID} onClick={()=> props.handleLocationClick(location.venueID)}><a>{location.title}</a></li>
+	);
 
     return (
       <div>
       <form>
         <label>
-          <input type="text" id="myInput" />
+          <input type="text" id="myInput" placeholder="Search" onChange={(event) => props.handleTextFilter(event)} />
           <ul id="myUL">
             {locationsList}
           </ul>
@@ -24,6 +22,5 @@ function FilterForm(props){
   );
 
 }
-
 
 export default FilterForm;
